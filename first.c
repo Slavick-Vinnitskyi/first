0226cf5d1bc894a4dea8dd16af25e9cae80e122e
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2017-04-02 22:11:19 +0300

    Cel->Fahr

--- first.c
+++ first.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
+#define Bot_temp 0
+#define Top_temp 300
+#define Step 20
 
-int main (void)
+int main()
 {
-    for(int i=0;i<10;i++)
-    printf("Hello world!\n");
-    
+    printf("Table Celsius->Fahrengeit\n");
+    printf("Celsius\tFahrengeit\n");
+ for(float Cel = Bot_temp; Cel<=Top_temp;Cel+=Step)
+ {
+     printf("%3.0f\t%3.0f\n",Cel, (((Cel*9)/5)-32));
+ } 
+ 
 }
