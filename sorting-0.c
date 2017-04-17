ec5d01afd5e7c657a8c48c161dcb95445403ebd1
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2017-04-17 23:07:53 +0300

    Сортировка пузырьком

---
+++ sorting-0.c
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+    srand(time(NULL));
+    int temp, a = 0; cin >> a;
+    int* mas = new int[a];
+    for(int i=0 ; i < a; i++)
+    {
+        mas[i] = rand() % 100 + 1; // 1...100   
+    }
+    cout<< "There are the random massive of "<< a<< " elements: ";
+    for(int j=0;j<a;j++)
+    {
+        cout<< mas[j]<<" ";
+    }cout<<endl;
+   for(int i = 0 ; i < a; i++)
+   {     
+   for(int k = 0 ; k < a - 1; k++ )
+    {
+     if(mas[k] > mas[k+1])
+        {
+        temp = mas[k];             
+        mas[k] = mas[k+1];
+        mas[k+1] = temp;
+
+        }
+    }
+   }
+   cout<< "There are the sorted massive of "<< a<< " elements: ";
+    for(int j=0;j<a;j++)
+    {
+        cout<< mas[j]<<" ";
+    }cout<<endl;
+delete [] mas;
+}
